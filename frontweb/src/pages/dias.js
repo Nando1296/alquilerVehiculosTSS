37a4a87e19b79d7778441147fa0d1ba5ab27b67d
Kevin Calani <kevincalani2311@gmail.com>
2023-11-20 17:18:51 -0600

    añadiendo dias, renta

--- frontweb/src/pages/dias.js
+++ frontweb/src/pages/dias.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import '../assets/form.css'
 
 export default class Dias extends Component {
   render() {
     return (
-      <div>
-        dias
+      <div class='main-center'>
+        <form class="" method="post" action="#">
+						
+						<div class="form-group">
+							<label for="name">N° de Dias de Renta</label>
+								<div class="input-group">
+				<input type="text" class="form-control"/>
+							</div>
+						</div>
+
+            <div class="ml-auto">
+				<button id="btn" class="btn btn-primary" type="">Guardar</button>
+						</div>
+            </form>
+
+            <form class="" method="post" action="#">
+						
+						<div class="form-group">
+							<label for="name">N° de Dias de Renta</label>
+								<div class="input-group">
+				<input type="text" class="form-control"/>
+							</div>
+						</div>
+
+            <div class="ml-auto">
+				<button id="btn" class="btn btn-primary" type="">Guardar</button>
+						</div>
+            </form>
       </div>
     )
   }
--- frontweb/src/pages/renta.js
+++ frontweb/src/pages/renta.js
@@ -1,10 +1,132 @@
 import React, { Component } from 'react'
+import "./renta.css"
 
 export default class Renta extends Component {
   render() {
     return (
       <div>
-        renta
+
+
+            <body>
+
+            <div class="table-container" id='renta'>
+
+        
+      <br></br>
+      <table>
+        <thead>
+            <tr>
+                <th>N° de dias Rentados por auto</th>
+                <th>4</th>
+            
+            </tr>
+        </thead>
+      
+      </table>
+
+
+      <table>
+        <thead>
+        <tr>
+                <th>N° de autos rentados por dia</th>
+                <th>4</th>
+                </tr>
+        </thead>
+      
+      </table>
+      <br></br>
+      </div>
+
+      <div class="table-container" id='renta'>
+
+        
+            <br></br>
+          <table>
+              <thead>
+                  <tr>
+                      <th>N° de dias Rentados por auto</th>
+                      <th>Probabilidad acumulada</th>
+                      <th>Probabilidad</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td>0.4</td>
+                      <td>0.4</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>2</td>
+                      <td>0.75</td>
+                      <td>0.35</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>3</td>
+                      <td>0.9</td>
+                      <td>0.15</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>4</td>
+                      <td>1</td>
+                      <td>0.1</td>
+                    
+                  </tr>
+                
+                
+              </tbody>
+          </table>
+
+        
+          <table>
+              <thead>
+              <tr>
+                      <th>N° de autos rentados por dia</th>
+                      <th>Probabilidad acumulada</th>
+                      <th>Probabilidad</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>0</td>
+                      <td>0.1</td>
+                      <td>0.10</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>1</td>
+                      <td>0.2</td>
+                      <td>0.10</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>2</td>
+                      <td>0.25</td>
+                      <td>0.45</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>3</td>
+                      <td>0.75</td>
+                      <td>0.30</td>
+                    
+                  </tr>
+                  <tr>
+                      <td>4</td>
+                      <td>1</td>
+                      <td>0.75</td>
+                    
+                  </tr>
+                  
+                  
+              </tbody>
+          </table>
+          <br></br>
+      </div>
+
+</body>
       </div>
     )
   }
