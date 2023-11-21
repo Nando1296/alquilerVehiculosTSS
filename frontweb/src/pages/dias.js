4fdbf06c54fe09f3a9049d95af4acc7137d43469
jhen26 <jhennyreque@gmail.com>
2023-11-21 10:54:58 -0400

    caso1

--- frontweb/src/pages/dias.js
+++ frontweb/src/pages/dias.js
@@ -4,6 +4,9 @@ import '../assets/form.css'
 export default class Dias extends Component {
   render() {
     return (
+		<div>
+        <div class="container">
+			<div class="main">
       <div class='main-center'>
         <form class="" method="post" action="#">
 						
@@ -14,14 +17,12 @@ export default class Dias extends Component {
 							</div>
 						</div>
 
-            <div class="ml-auto">
-				<button id="btn" class="btn btn-primary" type="">Guardar</button>
-						</div>
             </form>
 
             <form class="" method="post" action="#">
 						
 						<div class="form-group">
+							<br></br>
 							<label for="name">N° de Dias de Renta</label>
 								<div class="input-group">
 				<input type="text" class="form-control"/>
@@ -33,6 +34,9 @@ export default class Dias extends Component {
 						</div>
             </form>
       </div>
+	  </div>
+	  </div>
+	  </div>
     )
   }
 }
--- frontweb/src/pages/inicio.js
+++ frontweb/src/pages/inicio.js
@@ -1,97 +1,32 @@
 import React, { Component } from 'react'
-import '../assets/stylestabla.css'
+import '../assets/inicio.css'
 
 export default class Inicio extends Component {
   render() {
     return (
       <div>
-        <h1>Datos iniciales</h1>
 
-        <div class="container">
-        <div id="izq">
-                <h4>Numero de dias rentados:</h4>
-                <input type="number" placeholder="Cant Dias" required></input>
-                <p></p>
-                <table id="tabla-autos">
-                    <thead>
-                        <tr>
-                            <th>N°Autos rentados por dia</th>
-                            <th>Probabilidad</th>
-                            <th>Probabilidad acumulada</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>0.40</td>
-                            <td>0.40</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>0.35</td>
-                            <td>0.75</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>0.15</td>
-                            <td>0.90</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>0.10</td>
-                            <td>1</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+        <h2>Caso de estudio 1</h2>
+        <h1>Empresa de Alquiler de Autos</h1>
 
-            <div id="der">
-                <h4>Número de autos:</h4>
-                <input type="number" placeholder="Cant Autos" required></input>
-                <p></p>
-                <table id="tabla-autos">
-                    <thead>
-                        <tr>
-                            <th>N°Autos rentados por dia</th>
-                            <th>Probabilidad</th>
-                            <th>Probabilidad acumulada</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>0</td>
-                            <td>0.10</td>
-                            <td>0.10</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>0.10</td>
-                            <td>0.20</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>0.25</td>
-                            <td>0.45</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>0.30</td>
-                            <td>0.75</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>0.25</td>
-                            <td>1</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <p></p>
+         <p >
+          La empresa de alquiler de automóviles está tratando de determinar la cantidad óptima de
+          automóviles que comprará. El precio promedio anual de un automóvil es de 75.000 Bs. Por
+          cierto. Esta empresa ha recopilado las siguientes probabilidades de operacion:
+         <br>
+         
+         
+         </br>
+          Si el alquiler diario por auto es de Bs. 350, el costo de no tener un auto disponible cuando
+          se está solicitando es de Bs. 200, y el costo de tener un carro ocioso durante un día es de
+          Bs. 50, ¿Qué cantidad de automóviles debería comprar la empresa? Asuma que un
+          automóvil que se alquila puede estar alquilado durante varios días y que una vez devuelto
+          estará disponible al día siguiente. También, asuma 365 días de operación al año.
+        
 
-                <button>Siguiente</button>
-
-            </div>    
-
-        </div>
+        </p>
+        <br></br>
+        <img src="alquiler.png" alt=''></img>
       </div>
     )
   }
